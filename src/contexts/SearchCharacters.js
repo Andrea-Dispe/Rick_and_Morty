9e6a7608357e5fde6a4594a833a2725efe9f4c01
Andrea <dispe.andrea@gmail.com>
2022-06-25 01:34:12 +0200

    Search added

---
+++ src/contexts/SearchCharacters.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchCharactersContext = React.createContext(null);
+
+export { SearchCharactersContext };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
+import React, { useContext } from 'react'
+import { SearchCharactersContext } from '../contexts/SearchCharacters'
+
+
 import Card from "../components/Card"
 
 const Home = ({ characters }) => {
+  const { query, setQuery } = useContext(SearchCharactersContext);
 
   return (
     <>
+      <div className="search">
+        <input type="text"
+          placeholder={"Search Character"}
+          className={"input"}
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+        />
+      </div>
       <div className="cards-wrapper">
         {characters && characters.length > 0 ?
           characters.map(char => {
