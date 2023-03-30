0182e2e16dc8e117a900869ca095702850c2ea9a
Andrea-Dispe <dispe.andrea@gmail.com>
2023-03-30 11:09:15 +0200

    Added clear search filter

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,15 @@
 import { useContext } from 'react';
 import { SearchContext } from '../contexts/SearchContext'
 import { FcSearch } from "react-icons/fc";
+import { ImCross } from "react-icons/im";
+
 import './SearchBar.css'
 
 
 const SearchBar = () => {
   const { query, setQuery } = useContext(SearchContext);
   return (
+    <>
     <div className="search-wrapper">
       <div className="search">
         <input type="text"
@@ -16,8 +19,11 @@ const SearchBar = () => {
           value={query}
         />
         <FcSearch className="search-icon"/>
+        {query ?  <ImCross className='red-xmark' onClick={e => setQuery('')} /> : ''}
       </div>
     </div>
+    {/* <button onClick={e => setQuery('')} className="clear-filter">Clear search</button> */}
+    </>
   );
 }
 
